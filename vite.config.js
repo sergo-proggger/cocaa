fabd14005e9d106f8193f494aaed5b6c037af1a3
Sergey <sergey.cherokee@gmail.com>
2026-01-07 01:35:51 +0300

    created feauters

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,34 @@
 import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   root: './src',
+  base: '/coca/',
   server: {
     port: 3000,
-    open: true,
   },
+  publicDir: './src/assets',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+      },
+    },
   },
+  plugins: [
+    injectHTML(),
+    ViteMinifyPlugin(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+  ],
 });
