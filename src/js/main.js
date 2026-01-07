2160b3f4b6033ab14ab80cbea32c8e2f2fb1aecd
Sergey <sergey.cherokee@gmail.com>
2026-01-07 15:55:54 +0300

    created testimonials

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,9 @@ import '/scss/main.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
-import { useInsightSlider } from './components/slider.js';
+import { useInsightSlider, useTestimonialsSlider } from './components/home/slider.js';
 
 useTheme();
 useBurger();
 useInsightSlider();
+useTestimonialsSlider();
