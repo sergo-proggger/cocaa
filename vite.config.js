19c7172f6ca0690e39d53a9565e01b7f392e8fa1
Sergey <sergey.cherokee@gmail.com>
2026-01-07 17:26:33 +0300

    Build successful after removing html-inject plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
-import injectHTML from 'vite-plugin-html-inject';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   root: './src',
-  base: '/coca/',
+  base: '/cocaa/',
   server: {
     port: 3000,
   },
@@ -22,7 +21,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    injectHTML(),
     ViteMinifyPlugin(),
     ViteImageOptimizer({
       png: {
