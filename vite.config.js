2618f67abd0a8367e8aaa6859b4b37c142a93840
Sergey <sergey.cherokee@gmail.com>
2026-01-07 16:52:42 +0300

    created contact get-started

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
       input: {
         main: './src/index.html',
         pricing: './src/pricing.html',
+        contact: './src/contact.html',
       },
     },
   },
